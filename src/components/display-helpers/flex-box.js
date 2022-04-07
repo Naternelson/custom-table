4cb65800321bffa30243dd719328ce23a5bfa4e9
Nate <7nater.nelson@gmail.com>
2022-04-07 14:16:09 -0600

    Documentation for FlexBox

--- src/components/display-helpers/flex-box.js
+++ src/components/display-helpers/flex-box.js
@@ -3,6 +3,7 @@ import { Box } from "@mui/material"
 export default function FlexBox(p){
     const props = {...p}
     if(props.direction && !props.flexDirection) props.flexDirection = props.direction
+    if(props.justify && !props.justifyContent) props.justifyContent = props.justify
     let sx = {display:'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'stretch', alignContent: 'center'}
     Object.keys(sx).forEach(k => sx[k] = props[k] ? props[k] : sx[k])
     if(props.rowGap) sx.rowGap = props.rowGap
@@ -14,3 +15,10 @@ export default function FlexBox(p){
         </Box>
     )
 }
+
+// ====================
+// FlexBox
+// A Styled Box Component that is of display 'flex' by default
+// Flex attributes can be applied directly to the props of FlexBox or in the sx prop
+// Alias for flexDirection - direction && justifyContent - justify 
+// ====================
